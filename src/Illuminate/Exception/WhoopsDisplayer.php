561896231081e686c00ef7e12fa74badbbd0d480
xix <jose@latouraineinc.com>
2017-11-20 16:03:12 -0800

    Update WhoopsDisplayer.php

--- src/Illuminate/Exception/WhoopsDisplayer.php
+++ src/Illuminate/Exception/WhoopsDisplayer.php
@@ -40,7 +40,7 @@ class WhoopsDisplayer implements ExceptionDisplayerInterface {
 	 * @param  \Exception  $exception
 	 * @return \Symfony\Component\HttpFoundation\Response
 	 */
-	public function display(\Throwable $exception)
+	public function display(Exception $exception)
 	{
 		$status = $exception instanceof HttpExceptionInterface ? $exception->getStatusCode() : 500;
 
